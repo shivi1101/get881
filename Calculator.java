06adf836163f8f2e04de50364324de50571e3f20
shivi1101 <62545678+shivi1101@users.noreply.github.com>
2020-12-17 13:46:42 +0530

    second commit
    updated diff method

--- Calculator.java
+++ Calculator.java
@@ -2,9 +2,13 @@ class Calculator{
    public static int add(int n1,int n2){
    return n1+n2;
    }
+   public static int diff(int n3,int n4){
+   return n3+n4;
+   }
    public static void main(String []args){
    
    System.out.println(add(10,10));
+      System.out.println(diff(10,10));
    
    }
 }
