08334a9c0fe42cbe3fbfcc03c1d78b314ad2d515
shivi1101 <62545678+shivi1101@users.noreply.github.com>
2020-12-17 13:39:42 +0530

    first commit
    created calculator with add method

---
+++ Calculator.java
@@ -0,0 +1,10 @@
+class Calculator{
+   public static int add(int n1,int n2){
+   return n1+n2;
+   }
+   public static void main(String []args){
+   
+   System.out.println(add(10,10));
+   
+   }
+}
