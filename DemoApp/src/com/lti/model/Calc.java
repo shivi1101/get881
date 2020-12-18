fd4bf0733c8ec161cdd6ce7f756148df704bd6a8
shivi1101 <62545678+shivi1101@users.noreply.github.com>
2020-12-18 13:05:31 +0530

    added difference method
    hi

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -5,7 +5,7 @@ public class Calc {
 	public void add(int n1, int n2) {
 		System.out.println("Sum= "+(n1+n2));
 	}
-	public void diff(int n3, int n4) {
-		System.out.println("Diff= "+(n3-n4));
+	public void difference(int n3, int n4) {
+		System.out.println("Difference= "+(n3-n4));
 	}
 }
